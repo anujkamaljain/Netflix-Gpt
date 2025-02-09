448ededb9b3b528ff8c15de41c2feb4e7d26ce32
anujkamaljain <23UCS540@lnmiit.ac.in>
2025-02-10 02:23:02 +0530

    Created project using vite and did planning.

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import React from 'react';
+import ReactDOM from "react-dom/client"
+
+function App() {
+  return (
+  <div>
+    <h1 className='text-3xl'>Hello World!</h1>
+  </div>
+  )
+}
+
+export default App
